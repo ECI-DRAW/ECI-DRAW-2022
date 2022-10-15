4af954345b91462e9a3882679ecbe969626fa6db
Santiago Fetecua <santiago.fetecua@mail.escuelaing.edu.co>
2022-10-15 13:25:10 -0500

    First commit

---
+++ src/main/java/edu/eci/arsw/ecidraw/EciDrawApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecidraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciDrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/ecidraw/EciDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecidraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciDrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
