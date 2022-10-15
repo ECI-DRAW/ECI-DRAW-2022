f471250f60eccf3c54cb5f9b154aefdbb53f93d8
Santiago Fetecua <santiago.fetecua@mail.escuelaing.edu.co>
2022-10-15 14:12:31 -0500

    fixed pom, webapp running

--- src/main/java/edu/eci/arsw/ecidrawapi/EciDrawApplication.java
+++ src/main/java/edu/eci/arsw/ecidrawapi/EciDrawApplication.java
@@ -1,9 +1,11 @@
-package edu.eci.arsw.ecidraw;
+package edu.eci.arsw.ecidrawapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "edu.eci.arsw.ecidraw" })
 public class EciDrawApplication {
 
 	public static void main(String[] args) {
