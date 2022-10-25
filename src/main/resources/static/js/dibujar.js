1d3a76190f956928f03f1db55732e5f863bf0756
Santiago Fetecua <santiago.fetecua@mail.escuelaing.edu.co>
2022-10-25 02:03:28 -0500

    modo adivinar, ajustes generales

---
+++ src/main/resources/static/js/dibujar.js
@@ -0,0 +1,77 @@
+var paint = (0, 0, 0);
+var stomp;
+let lineWidth = 3;
+
+function setup() {
+    createCanvas(850, 430);
+    stomp();
+}
+
+function draw() {
+    if (mouseIsPressed === true) {
+        fill(paint);
+        stroke(paint);
+        strokeWeight(lineWidth);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+        var json = {
+            xPos: mouseX,
+            yPos: mouseY,
+            pxPos: pmouseX,
+            pyPos: pmouseY,
+            colors: paint,
+            lwidth: lineWidth,
+            eraser: false,
+        }
+        refresh(json);
+    }
+}
+
+function stomp() {
+    var socket = new SockJS("/stompEndpoint");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        stompClient.subscribe("/topic/board", function (event) {
+            var json = JSON.parse(event.body);
+            if (!json.eraser) {
+                fill(json.colors);
+                stroke(json.colors);
+                strokeWeight(json.lwidth);
+                line(json.xPos, json.yPos, json.pxPos, json.pyPos);
+
+            } else {
+                clear();
+            }
+        });
+    });
+}
+
+function colorSetter() {
+    paint = document.getElementById("paint").value;
+}
+
+function colorEraser() {
+    paint = (241, 241, 241);
+}
+
+function clearBoard() {
+    clear();
+    var json = {
+        eraser: true,
+    }
+    refresh(json);
+}
+
+function refresh(json) {
+    stompClient.send("/topic/board", {}, JSON.stringify(json));
+}
+
+function lineWidthSetter() {
+    lineWidth = document.getElementById("lineWidth").value;
+    strokeWeight(lineWidth);
+}
+
+application.addEventListener('change', (e) => {
+    if (e.target.id === 'line-width') {
+        lineWidth = e.target.value;
+    }
+});
